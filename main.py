a340b2e29b1849766844e000c9d0806967586654
FerronHooi <ferron@live.nl>
2022-06-20 20:25:24 +0200

    Select random item from list added

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
+import random
 
 #Loading CSV in Pandas DataFrame
 my_csv = pd.read_csv('Dates 10f26e397f9c4b6da70d310139e5cd02.csv')
 activityNames = my_csv.Name
 
-for activity in activityNames:
-    print(activity)
+#Selects random item from list
+print(random.choice(activityNames))

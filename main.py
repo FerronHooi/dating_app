955de3bad43cbe6c98f30fb6c23dde038858c3f2
FerronHooi <ferron@live.nl>
2022-06-20 20:39:51 +0200

    Added application_ui.py, this should later include the user interface for the (web)app

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ activityStatus = my_csv.Status
 activityDurage = my_csv.Duur\
     .tolist()
 activityLocation = my_csv.Locatie.tolist()
+Tijdsduur = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24)
 
 #use inquirer to let user select from list
 """questions = [

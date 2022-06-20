ff2b3eba4e4f9145d89cb716553abc76dce17be0
FerronHooi <ferron@live.nl>
2022-06-20 20:23:49 +0200

    Loading CSV in Pandas DataFrame

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#Loading CSV in Pandas DataFrame
+my_csv = pd.read_csv('Dates 10f26e397f9c4b6da70d310139e5cd02.csv')
+activityNames = my_csv.Name
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for activity in activityNames:
+    print(activity)

66f6c3002eab2c65276d4f41cca86bb44ce62fee
Ferron Hooi <102597092+FerronHooiInergy@users.noreply.github.com>
2022-06-21 20:36:09 +0200

    User can now select a durage/location/status etc by entering a number

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import random
 from collections import Counter
 
+# TODO: Also add column which user added which activity --> in order to also be able to select based on who
 Tijdsduur = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24)
 
 #Loading CSV in Pandas DataFrame
@@ -11,14 +12,15 @@ my_csv = pd.read_csv('Dates 10f26e397f9c4b6da70d310139e5cd02.csv')
 def activityProperty(csv_column):
     activityProperty = csv_column.tolist()
     activityProperty = sorted(list(set([item for item in activityProperty if not (pd.isnull(item)) == True])))
-    print(activityProperty)
+    #print(activityProperty)
+    return(activityProperty)
 
 #fumction below shows the count of each occurence
 def activityPropertyCount(csv_column):
     activityProperty = csv_column.tolist()
     activityCount = Counter(sorted(list([item for item in activityProperty if not (pd.isnull(item)) == True])))
-    print(activityCount)
-
+    #print(activityCount)
+    return(activityCount)
 
 activityLocation = activityProperty(my_csv.Locatie)
 activityLocationCount = activityPropertyCount(my_csv.Locatie)
@@ -32,22 +34,40 @@ activityStatusCount = activityPropertyCount(my_csv.Status)
 activityDurage = activityProperty(my_csv.Duur)
 activityDurageCount = activityPropertyCount(my_csv.Duur)
 
-# TODO: Fix inquirer
-#use inquirer to let user select from list
-"""questions = [
-  inquirer.List('size',
-                message="What is the durage you want to get a random activity from?",
-                choices=activityDurage,
-            ),
-]
-answers = inquirer.prompt(questions)
-print (answers["size"])"""
-
 # TODO: If user executes this scripts then
     # 1. He should pick a status (or not)
     # 2. He should then pick a max duration (or not)
     # 3. He should be asked to pick a location (or not)
 
+def let_user_pick(options):
+    print("Please choose:")
 
+    for idx, element in enumerate(options):
+        print("{}) {}".format(idx + 1, element))
+
+    i = input("Enter number: ")
+    try:
+        if 0 < int(i) <= len(options):
+            return int(i) - 1
+    except:
+        pass
+    return None
+
+def selection(activityPropertyList):
+    nr = let_user_pick(activityPropertyList)
+    selection = activityPropertyList[nr]
+    print("You chose: " + selection)
+    return selection
+
+
+chosenStatus = selection(activityStatus)
+chosenDurage = selection(activityDurage)
+chosenLocation = selection(activityLocation)
+
+print(chosenStatus)
+print(chosenDurage)
+print(chosenLocation)
+
+print("You have chosen", chosenStatus, "which will take", chosenDurage, "hours and take place in", chosenLocation)
 #Selects random item from list
-print(random.choice(activityNames))
+#print(random.choice(list(activityNames)))

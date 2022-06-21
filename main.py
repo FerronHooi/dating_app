176cf34d4c7a081e5bdb96a789a1280fc85a446f
Ferron Hooi <102597092+FerronHooiInergy@users.noreply.github.com>
2022-06-21 19:53:25 +0200

    Added activityProperty function to remove nan from list and only show unique values in the list

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import pandas as pd
 import random
-import inquirer
 
-
-#Loading CSV in Pandas DataFrame
-my_csv = pd.read_csv('Dates 10f26e397f9c4b6da70d310139e5cd02.csv')
-activityNames = my_csv.Name
-activityStatus = my_csv.Status
-activityDurage = my_csv.Duur\
-    .tolist()
-activityLocation = my_csv.Locatie.tolist()
 Tijdsduur = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24)
 
+# TODO: Show count of each individual activityS/D/N
+#Loading CSV in Pandas DataFrame
+my_csv = pd.read_csv('Dates 10f26e397f9c4b6da70d310139e5cd02.csv')
+
+# TODO: Get only distinct value from lists beneath
+
+#function below removes nan from list and shows only unique values in the list
+def activityProperty(csv_column):
+    activityProperty = csv_column.tolist()
+    activityProperty = list(set([item for item in activityProperty if not (pd.isnull(item)) == True]))
+    print(activityProperty)
+
+activityLocation = activityProperty(my_csv.Locatie)
+activityNames = activityProperty(my_csv.Name)
+activityStatus = activityProperty(my_csv.Status)
+activityDurage = activityProperty(my_csv.Duur)
+
+
+
+
+# TODO: Fix inquirer
 #use inquirer to let user select from list
 """questions = [
   inquirer.List('size',
@@ -21,6 +33,11 @@ Tijdsduur = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 
 ]
 answers = inquirer.prompt(questions)
 print (answers["size"])"""
+
+# TODO: If user executes this scripts then
+    # 1. He should pick a status (or not)
+    # 2. He should then pick a max duration (or not)
+    # 3. He should be asked to pick a location (or not)
 
 
 #Selects random item from list
